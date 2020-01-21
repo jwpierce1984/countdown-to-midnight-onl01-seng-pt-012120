8c274904c305a4a03affbe6bd14e96d0cf8ac1fa
Justin Pierce <jwpierce1984@gmail.com>
2020-01-21 22:08:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,10 +1,19 @@
 #write your code here
-sleep(num_secs)
 def countdown (seconds)
   seconds = 10
   while seconds > 0
   puts "#{seconds} SECOND(S)!"
   seconds -= 1
+end
+"HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep (seconds)
+  seconds = 10
+  while seconds > 0
+  puts "#{seconds} SECOND(S)!"
+  seconds -= 1
+  sleep(1.0)
 end
 "HAPPY NEW YEAR!"
 end
